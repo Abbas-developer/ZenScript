1c1865f6518d94a881f2f3a8caa59e1c0937e63e
Abbas-developer <139618533+Abbas-developer@users.noreply.github.com>
2023-08-24 21:25:11 +0500

    Add files via upload

--- error.py
+++ error.py
@@ -14,12 +14,24 @@ class SyntaxError(Error):
     def __init__(self, about, line_number=None):
         super().__init__("SyntaxError", about)
         self.as_string(line_number)
+class TypeError(Error):
+    def __init__(self, about, line_number=None):
+        super().__init__("TypeError", about)
+        self.as_string(line_number)
 
-class UnknowTypeError(Error):
+class UnknownTypeError(Error):
     def __init__(self, about, line_number=None):
         super().__init__("UnknowTypeError", about)
         self.as_string(line_number)
 class UnknowDependenciesError(Error):
     def __init__(self, about, line_number=None):
         super().__init__("UnknowDependenciesError", about)
+        self.as_string(line_number)
+class InvalidCharactersError(Error):
+    def __init__(self, about, line_number=None):
+        super().__init__("InvalidCharactersError", about)
+        self.as_string(line_number)
+class ImportError(Error):
+    def __init__(self, about, line_number=None):
+        super().__init__("ImportError", about)
         self.as_string(line_number)
--- interpreter.py
+++ interpreter.py
@@ -1,15 +1,17 @@
 import error
 import sys
 import json
-import socket
+import os
 def check_dependencies():
-    global usesocket
+    global useos
     with open('dependencies.json') as file:
         rawdata=json.load(file)
     build_in_dependencies = rawdata["build-in-dependencies"]
     for dependencies in build_in_dependencies:
-        if dependencies == "socket":
-            usesocket=True
+        if dependencies == "os":
+            useos=True
+        elif is_empty(dependencies):
+            pass
         else:
             error.UnknowDependenciesError(f"not found this dependencies name {dependencies}")
             sys.exit()
@@ -18,63 +20,164 @@ def get_context(filename: str):
         context: list=file.read().splitlines()
     return context
 stack_var=[]
-def check_type(value,count):
-    type=None
-    if value.startswith('"') or value.startswith("'"):
-        if value.endswith('"') or value.endswith("'"):
-            type="str"
-        else:
-            error.SyntaxError(f"""'"' expected in end""", line_number=count)
-            sys.exit()
+if_stack=[]
+def check_type(value, pc):
+    # Check if it's an integer
+    if value.isdigit():
+        return "int"
+    # Check if it's a float
+    elif is_float(value):
+        return "float"
+    if value.startswith("math("):
+        return "math"
+    elif value.startswith("input("):
+        return "input"
+    elif value.startswith("os.getcwd("):
+        return "os.getcwd"
     else:
-        error.UnknowTypeError("The specified data type is not recognized",line_number=count)
-        sys.exit()
+        value = str(value)
+        if value.startswith('"') or value.startswith("'"):
+            if value.endswith('"') or value.endswith("'"):
+                return "str"
+            else:
+                error.SyntaxError(f"""'"' expected in end""", line_number=pc)
+                sys.exit()
+        elif value == "True" or value == "False":
+            return "bool"
+        else:
+            error.UnknownTypeError("The specified data type is not recognized", line_number=pc)
+            sys.exit()
 
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
     return type
 def get_string_context(string):
     string=string[1:-1]
     return string
-def Print(line,count):
-    n,text=line.split(" ",1)
-    text_type=check_type(text,count)
+def Input(line,pc):
+    n,text=line.split("input(",1)        
+    text=text.replace(")", "",-1)
+    text_type=check_type(text,pc)
+    if text_type == "str":
+        text=get_string_context(text)
+        r=input(text)
+        return f'"{r}"'
+    elif text_type == "bool":
+        r=input(text)
+        return r
+    elif text_type == "int":
+        r=input(text)
+        return r
+    elif text_type == "float":
+        r=input(text)
+        return r
+    elif text_type == "math":
+        expression = text.replace("math(", "").replace(")", "").replace("Print",'')  # Extract the expression
+        result = math(expression)
+        r=input(result)
+        return r
+def Print(line,pc):
+    n,text=line.split("Print(",1)        
+    text=text.replace(")", "",-1)
+    text_type=check_type(text,pc)
     if text_type == "str":
         text=get_string_context(text)
         print(text)
+    elif text_type == "bool":
+        print(text)
+    elif text_type == "int":
+        print(text)
+    elif text_type == "float":
+        print(text)
+    elif text_type == "math":
+        expression = text.replace("math(", "").replace(")", "").replace("Print",'')  # Extract the expression
+        result = math(expression)
+        print(result)
+def is_empty(string):
+    if string==" " or string=="\t" or string=="":
+        return True
+    else:
+        return False
+def concatenate(str1,str2):
+    str1=get_string_context(str1)
+    str2=get_string_context(str2)
+    return str1+str2
+def math(expression):
+    expression=expression.replace("Print(","").replace("math(", "").replace(")", "")
+    print(expression)
+    allowed_operators = '+-*/%'
+    allowed_chars = set('.)(0123456789 ' + allowed_operators)
+    if all(char in allowed_chars for char in expression):
+        if any(op in expression for op in allowed_operators):
+            try:
+                result = eval(expression)
+                return str(result)
+            except Exception as e:
+                return f"Error: {e}"
+        else:
+            return "Expression does not contain any allowed operators."
+    else:
+        error.InvalidCharactersError("Invalid characters or operators in the expression.")
+        sys.exit()
+def make_str(value):
+    return f"'{value}'"    
 def code(context: list):
-    count=1
+    global useos
+    pc=1
     for line in context:
-        if line.endswith(";") or line==" " or line=="\t":
+        line=str(line)
+
+        if line.endswith(";") or is_empty(line):
             line=line.replace(";",'')
         else:
-            error.SyntaxError(f"""';' expected in end""", line_number=count)
+            error.SyntaxError(f"""';' expected in end""", line_number=pc)
             sys.exit()
         for var_type,var_name,var_value in stack_var:
             line=line.replace(var_name,var_value)
         if line.startswith("//"):
            continue
-        if line.startswith("Print"):
-            Print(line,count)
-            count+=1
+        if line.startswith("input("):
+            Input(line,pc)
+            pc+=1
             continue
-        elif line.startswith("var"):    
+        if line.startswith("var"):    
             line=line.replace("var ","",1)
             name,value = line.split("=")
             name=name.strip()
+            line=line.replace(f"{name} =",'')
             var_type=None
             if value[0] ==' ':
                 value=value.replace(" ","",1)
-            var_type=check_type(value,count)
+            var_type=check_type(value,pc)
+            if var_type == "math":
+                value = math(value)
+            elif var_type == "input":
+                value=Input(value,pc)
+            if var_type == "os.getcwd" and useos:    
+                value=make_str(value.replace(f'os.getcwd(',os.getcwd()).replace(")",""))
+            else:
+                error.ImportError("import the os module to use os.getcwd function",pc)
             stack_var.append((var_type,name,value))
-        elif line.startswith("socket.send") and usesocket:
-            n,data = line.replace("socket.send ","",1)
-            type=check_type(data,count)
-            if type == "str":
-                data=get_string_context(data)
-            socket.socket.send(data)
-        count+=1
-
+        if "math(" in line:
+            result = math(line)
+            line = line.replace(f"math({line})",f'"{result}"',1)
+        if line.startswith("Print("):
+            Print(line,pc)
+            pc+=1
+            continue
+        if "os.getcwd(" in line and useos== True:
+            line=line.replace(f'os.getcwd()',os.getcwd())
+        pc+=1
 def run(filename: str):
-    usesocket=False
+    global useos
+    useos=False
     check_dependencies()
     context=get_context(filename)
     code(context)
+
+if __name__ == "__main__":
+    run("TEST/test.zs")
